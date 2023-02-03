23b3c47dabff725b6d01551d9652f63f64462d82
Супрун Илья Станиславович <suprunis@ares.tgc-2.ru>
2023-02-03 17:23:47 +0300

    modified:   pkg/gossh.go

--- pkg/gossh.go
+++ pkg/gossh.go
@@ -12,37 +12,6 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-func GetConfig(hostname, username, password string, commands []string) error {
-	var reConf = regexp.MustCompile(`(?s)Current configuration .*end`)
-	var reHost = regexp.MustCompile(`(?m)^hostname\s([-0-9A-Za-z_]+).?$`)
-
-	out, err := doSSHCommands(hostname, username, password, commands)
-	if err != nil {
-		return err
-	}
-
-	if reHost.Match(out) {
-		fname := string(reHost.FindSubmatch(out)[1])
-
-		log.Print(fname)
-
-		if reConf.Match(out) {
-			config := reConf.FindAll(out, -1)[0]
-			err := os.WriteFile(fname, config, 0644)
-			if err != nil {
-				log.Print(err)
-				return err
-			}
-		} else {
-			log.Print(hostname, "config not found")
-		}
-	} else {
-		log.Print(hostname, "hostname not found")
-	}
-	// time.Sleep(5 * time.Second)
-	return nil
-}
-
 func doSSHCommands(hostname, username, password string, commands []string) ([]byte, error) {
 	port := "22"
 
@@ -169,35 +138,45 @@ func doSSHCommands(hostname, username, password string, commands []string) ([]by
 
 }
 
+func GetConfig(hostname, username, password string, commands []string) error {
+	var reConf = regexp.MustCompile(`(?s)Current configuration .*end`)
+	var reHost = regexp.MustCompile(`(?m)^hostname\s([-0-9A-Za-z_]+).?$`)
+
+	out, err := doSSHCommands(hostname, username, password, commands)
+	if err != nil {
+		return err
+	}
+
+	if reHost.Match(out) {
+		fname := string(reHost.FindSubmatch(out)[1])
+
+		log.Print(fname)
+
+		if reConf.Match(out) {
+			config := reConf.FindAll(out, -1)[0]
+			err := os.WriteFile(fname, config, 0644)
+			if err != nil {
+				log.Print(err)
+				return err
+			}
+		} else {
+			log.Print(hostname, "config not found")
+		}
+	} else {
+		log.Print(hostname, "hostname not found")
+	}
+	// time.Sleep(5 * time.Second)
+	return nil
+}
+
 func GetUsers(hostname, username, password string, commands []string, ch chan string) error {
 	defer close(ch)
-	// var reConf = regexp.MustCompile(`(?s)Current configuration .*end`)
-	// var reHost = regexp.MustCompile(`(?m)^hostname\s([-0-9A-Za-z_]+).?$`)
 	var reUser = regexp.MustCompile(`(?m)^username\s([-0-9A-Za-z_]+)\s`)
 
 	out, err := doSSHCommands(hostname, username, password, commands)
 	if err != nil {
 		return fmt.Errorf("getusers %s: %w", hostname, err)
 	}
-
-	// if reHost.Match(out) {
-	// 	fname := string(reHost.FindSubmatch(out)[1])
-
-	// 	log.Print(fname)
-
-	// 	if reConf.Match(out) {
-	// 		config := reConf.FindAll(out, -1)[0]
-	// 		err := os.WriteFile(fname, config, 0644)
-	// 		if err != nil {
-	// 			log.Print(err)
-	// 			return err
-	// 		}
-	// 	} else {
-	// 		log.Print(hostname, "config not found")
-	// 	}
-	// } else {
-	// 	log.Print(hostname, "hostname not found")
-	// }
 
 	err = os.WriteFile(hostname+".log", out, 0644)
 	if err != nil {
